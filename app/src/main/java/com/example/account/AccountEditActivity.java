db198d9c0b8b11bff1933b3c78201dffe5aeee6f
chen-tyang <126060356+chen-tyang@users.noreply.github.com>
2024-06-26 15:15:11 +0800

    恢复AccountEditActivity页面的小数判断

--- app/src/main/java/com/example/account/AccountEditActivity.java
+++ app/src/main/java/com/example/account/AccountEditActivity.java
@@ -2,13 +2,10 @@ package com.example.account;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -24,21 +21,20 @@ public class AccountEditActivity extends AppCompatActivity {
 
     private Button buttonSave;
     private Button buttonDelete;
-    private Button buttonCancel;
     private AccountViewModel accountViewModel;
     private Account currentAccount;
     private EditText editTextAmount;
     private EditText editTextRemarks;
     private Spinner spinner;
-
     String [] array;
     int [] imageArray;
+    // 增加一个成员变量来存储 decimalPlaces
+    private int decimalPlaces;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account_edit);
 
-        //array读取res/strings里的type类型的选项
+//array读取res/strings里的type类型的选项
         //imageArray对应图片编号
         array = getResources().getStringArray(R.array.spinner_type);
         imageArray = new int[]{
@@ -53,27 +49,30 @@ public class AccountEditActivity extends AppCompatActivity {
                 R.drawable.part_time_job,
                 R.drawable.financial_management,
         };
-
+        setContentView(R.layout.activity_account_edit);
+        // 用新的 ImageArrayAdapter 替换你现有的 ArrayAdapter
         spinner = findViewById(R.id.spinner);
+        ImageArrayAdapter spinnerAdapter = new ImageArrayAdapter(this, R.layout.spinner_with_image, array, imageArray);
+        spinner.setAdapter(spinnerAdapter);
         editTextAmount = findViewById(R.id.editTextAmount);
         editTextRemarks = findViewById(R.id.editTextRemarks);
 
         buttonSave = findViewById(R.id.buttonSave);
         buttonDelete = findViewById(R.id.buttonDelete);
-        buttonCancel = findViewById(R.id.buttonCancel);
 
-        // 用新的 ImageArrayAdapter 替换你现有的 ArrayAdapter
-        spinner = findViewById(R.id.spinner);
-        ImageArrayAdapter spinnerAdapter = new ImageArrayAdapter(this, R.layout.spinner_with_image, array, imageArray);
-        spinner.setAdapter(spinnerAdapter);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_type, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
-        //从MainActivity获得accountId用于修改和删除
+        // 从 Intent 中获取 decimalPlaces 的值
         Intent intent = getIntent();
+        decimalPlaces = intent.getIntExtra("decimalPlaces", 2);
         int accountId = intent.getIntExtra("accountId", -1);
         // 初始化ViewModel
         accountViewModel = new ViewModelProvider(this).get(AccountViewModel.class);
 
-        //accountId!=-1代表点击的是已经存在于数据库的数据
+        //accountId!=-1代表我点击的是已经存在于数据库的数据
         if (accountId != -1) {
             LiveData<Account> accountToEdit = accountViewModel.getAccount(accountId);
             // 观察LiveData的记账数据，在数据变化时更新UI
@@ -96,7 +95,6 @@ public class AccountEditActivity extends AppCompatActivity {
                     }
                 }
             });
-            //点击保存按钮会替换原本的内容
             buttonSave.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -109,7 +107,8 @@ public class AccountEditActivity extends AppCompatActivity {
                     String amountString = editTextAmount.getText().toString();
                     double amount = Double.parseDouble(editTextAmount.getText().toString());
                     if (!isValidAmount(amountString)) {
-                        Toast.makeText(AccountEditActivity.this, "金额应当最多包含两位小数", Toast.LENGTH_SHORT).show();
+                        String toastMessage = String.format("金额应当最多包含%d位小数", decimalPlaces);
+                        Toast.makeText(AccountEditActivity.this, toastMessage, Toast.LENGTH_SHORT).show();
                         return;
                     }
                     accountToEdit.getValue().setAmount(amount);
@@ -124,12 +123,11 @@ public class AccountEditActivity extends AppCompatActivity {
                     finish();
                 }
             });
-            //删除按钮会删除该记录
             buttonDelete.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     // 如果currentAccount为null，说明观察的LiveData没有发生变化，或者Account不存在
-                    // 但这段代码不会执行，因为从列表点进来的都是已经存在的
+                    //但这段代码不会执行，因为从列表点进来的都是已经存在的
                     if (currentAccount == null) {
                         Toast.makeText(AccountEditActivity.this, "No account to be deleted", Toast.LENGTH_SHORT).show();
                         return;
@@ -141,14 +139,6 @@ public class AccountEditActivity extends AppCompatActivity {
                     finish();
                 }
             });
-            //如无需修改,点击取消按钮
-            buttonCancel.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // 直接返回到主Activity
-                    finish();
-                }
-            });
         } else {//代表accountId==-1,说明是新建的数据,而不是修改或者删除数据
             buttonSave.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -156,9 +146,9 @@ public class AccountEditActivity extends AppCompatActivity {
                     //先检查textview中的文本信息是否符合要求，然后再转换成double类型存储
                     String amountString = editTextAmount.getText().toString();
                     double amount = Double.parseDouble(amountString);
-                    //金额的小数检查
                     if (!isValidAmount(amountString)) {
-                        Toast.makeText(AccountEditActivity.this, "金额应当最多包含两位小数", Toast.LENGTH_SHORT).show();
+                        String toastMessage = String.format("金额应当最多包含%d位小数", decimalPlaces);
+                        Toast.makeText(AccountEditActivity.this, toastMessage, Toast.LENGTH_SHORT).show();
                         return;
                     }
                     String remarks = editTextRemarks.getText().toString();
@@ -173,11 +163,9 @@ public class AccountEditActivity extends AppCompatActivity {
                     finish();
                 }
             });
-            // 隐藏 buttonDelete
-            buttonDelete.setVisibility(View.GONE);
+            buttonDelete.setText("取消");
 
-            //取消按钮跳回MainActivity
-            buttonCancel.setOnClickListener(new View.OnClickListener() {
+            buttonDelete.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     // 直接返回到主Activity
@@ -191,9 +179,13 @@ public class AccountEditActivity extends AppCompatActivity {
     private boolean isValidAmount(String amountString) {
         int decimalIndex = amountString.indexOf(".");//用于查找金额字符串中小数点的位置。如果不存在小数点，indexOf返回-1。
         //decimalIndex确保字符串中存在小数点,amountString.length() - decimalIndex - 1 > 2计算小数点后的位数，并检查是否超过两位。
-        if (decimalIndex >= 0 && amountString.length() - decimalIndex - 1 > 2) {
-            return false;
+        if (decimalIndex >= 0) {
+            int decimalPlacesInAmount = amountString.length() - decimalIndex - 1;
+            if ((decimalPlaces == 2 && decimalPlacesInAmount > 2) || (decimalPlaces == 6 && decimalPlacesInAmount > 6)) {
+                return false;
+            }
         }
         return true;
     }
+
 }
